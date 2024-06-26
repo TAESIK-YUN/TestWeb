6b7a419f1ee34cab882728b3067cfab3ed66e90c
m2it <m2it@DESKTOP-AGD6PQM>
2024-06-26 14:59:05 +0900

    default

---
+++ photoalbum/src/test/java/com/demo/photoalbum/PhotoalbumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.photoalbum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoalbumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
